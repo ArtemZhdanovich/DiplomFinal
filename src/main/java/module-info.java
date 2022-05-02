9ad7ee5853ab8c0c8935a0f57e8a2583da003961
Artem Zhdanovich <artem.zhdanboro@gmail.com>
2022-05-02 14:17:40 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module zhdanboro.ui.diplomfinal {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens zhdanboro.ui.diplomfinal to javafx.fxml;
+    exports zhdanboro.ui.diplomfinal;
+}
